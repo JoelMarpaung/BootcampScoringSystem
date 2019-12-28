2ba64954ecbfc8bdc5d39ac92953f96490a9f3d0
Silvia Novita <silvianovita678@gmail.com>
2019-12-28 21:32:58 +0700

    Adding Create,update,datatable in Client/Grade

--- Client/Controllers/GradesController.cs
+++ Client/Controllers/GradesController.cs
@@ -5,6 +5,7 @@ using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using Data.Model;
+using Data.ViewModel;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
@@ -46,7 +47,62 @@ namespace Client.Controllers
             }
             return Json("Failed");
         }
+        public JsonResult Add(GradeVM grade)
+        {
+            var client = new HttpClient
+            {
+                BaseAddress = new Uri(Baseurl)
+            };
+            var myContent = JsonConvert.SerializeObject(grade);
+            var buffer = System.Text.Encoding.UTF8.GetBytes(myContent);
+            var byteContent = new ByteArrayContent(buffer);
+            byteContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+            var result = client.PostAsync("api/Grade/", byteContent).Result;
 
+            return Json(result);
+        }
+
+        public JsonResult Update(int id, GradeVM grade)
+        {
+            var client = new HttpClient
+            {
+                BaseAddress = new Uri(Baseurl)
+            };
+            var myContent = JsonConvert.SerializeObject(grade);
+            var buffer = System.Text.Encoding.UTF8.GetBytes(myContent);
+            var byteContent = new ByteArrayContent(buffer);
+            byteContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+            var result = client.PutAsync("api/Grade/" + id, byteContent).Result;
+            return Json(result);
+
+        }
+
+        public async Task<JsonResult> GetbyID(int id)
+        {
+            HttpResponseMessage response = await client.GetAsync("api/Grade");
+            if (response.IsSuccessStatusCode)
+            {
+                var data = await response.Content.ReadAsAsync<Grade[]>();
+                var attitude = data.FirstOrDefault(s => s.Id == id);
+                var json = JsonConvert.SerializeObject(attitude, Formatting.None, new JsonSerializerSettings()
+                {
+                    ReferenceLoopHandling = ReferenceLoopHandling.Ignore
+                });
+
+                return Json(json);
+            }
+            return Json("Internal Server Error");
+        }
+
+        public JsonResult Delete(int id)
+        {
+            var client = new HttpClient
+            {
+                BaseAddress = new Uri(Baseurl)
+            };
+            var result = client.DeleteAsync("api/Grade/" + id).Result;
+            return Json(result);
+        }
         // GET: Grades/Details/5
         public ActionResult Details(int id)
         {
@@ -98,13 +154,7 @@ namespace Client.Controllers
                 return View();
             }
         }
-
-        // GET: Grades/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
+        
         // POST: Grades/Delete/5
         [HttpPost]
         [ValidateAntiForgeryToken]
