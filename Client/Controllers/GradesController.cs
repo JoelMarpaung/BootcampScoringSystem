c108b677e179e936e399b8ec3af79471b37b56db
Silvia Novita <silvianovita678@gmail.com>
2019-12-28 21:25:32 +0700

    Update Grade Repository

--- Client/Controllers/GradesController.cs
+++ Client/Controllers/GradesController.cs
@@ -1,18 +1,50 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
+using Data.Model;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace Client.Controllers
 {
     public class GradesController : Controller
     {
+        string Baseurl = "https://localhost:44373/";
+        readonly HttpClient client = new HttpClient();
+        public GradesController()
+        {
+            //pasing service baseurl
+            client.BaseAddress = new Uri(Baseurl);
+            //clear default request header
+            client.DefaultRequestHeaders.Clear();
+            //define request data format
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+        }
         // GET: Grades
         public ActionResult Index()
         {
             return View();
+        }
+        public async Task<JsonResult> List()
+        {
+            HttpResponseMessage Res = await client.GetAsync("api/Grade");
+            if (Res.IsSuccessStatusCode)
+            {
+                var service = await Res.Content.ReadAsAsync<Grade[]>();
+
+                //Deserializing the response received from web api 
+                var json = JsonConvert.SerializeObject(service, Formatting.None, new JsonSerializerSettings()
+                {
+                    ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
+                });
+                return Json(json);
+            }
+            return Json("Failed");
         }
 
         // GET: Grades/Details/5
