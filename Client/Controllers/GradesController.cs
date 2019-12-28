5dbc2df119bd79b64dd8fa3a0bc57297c1f8287f
joelmarpaung <joelmarpaung@outlook.com>
2019-12-28 14:48:12 +0700

    Add empty master data controller and view

---
+++ Client/Controllers/GradesController.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Client.Controllers
+{
+    public class GradesController : Controller
+    {
+        // GET: Grades
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Grades/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Grades/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Grades/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Grades/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Grades/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Grades/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Grades/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
