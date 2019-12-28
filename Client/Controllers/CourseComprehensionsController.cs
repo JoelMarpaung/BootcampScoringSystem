2b390045b87df564491d3b34e17fb68060ab0eca
Annisanofitriandi <nofitriandiannisa@gmail.com>
2019-12-28 22:15:16 +0700

    Adding CourseComprehension

--- Client/Controllers/CourseComprehensionsController.cs
+++ Client/Controllers/CourseComprehensionsController.cs
@@ -1,19 +1,65 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
+using Data.Model;
+using Data.ViewModel;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace Client.Controllers
 {
     public class CourseComprehensionsController : Controller
     {
+        string Baseurl ="https://localhost:44373/";
+        readonly HttpClient client = new HttpClient();
+
+        public CourseComprehensionsController()
+        {
+            client.BaseAddress = new Uri(Baseurl);
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
         // GET: CourseComprehensions
         public ActionResult Index()
         {
-            return View();
+            return View(List());
         }
+
+
+        public async Task<JsonResult> List()
+        {
+            HttpResponseMessage response = await client.GetAsync("api/CourseComprehension");
+            if (response.IsSuccessStatusCode)
+            {
+                var data = await response.Content.ReadAsAsync<CourseComprehension[]>();
+                var json = JsonConvert.SerializeObject(data, Formatting.None, new JsonSerializerSettings()
+                {
+                    ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
+                });
+                return Json(json);
+            }
+            return Json("Internal Server Error");
+        }
+
+        //public async Task<JsonResult> List()
+        //{
+        //    HttpResponseMessage response = await client.GetAsync("API/Class");
+        //    if (response.IsSuccessStatusCode)
+        //    {
+        //        var data = await response.Content.ReadAsAsync<CourseComprehension[]>();
+        //        var json = JsonConvert.SerializeObject(data, Formatting.None, new JsonSerializerSettings()
+        //        {
+        //            ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
+        //        });
+        //        return Json(json);
+        //    }
+        //    return Json("Internal Server Error");
+        //}
 
         // GET: CourseComprehensions/Details/5
         public ActionResult Details(int id)
@@ -67,10 +113,33 @@ namespace Client.Controllers
             }
         }
 
-        // GET: CourseComprehensions/Delete/5
-        public ActionResult Delete(int id)
+        public async Task<JsonResult> GetbyId(int id) //Update row
         {
-            return View();
+            HttpResponseMessage response = await client.GetAsync("API/CourseComprehension");
+            if (response.IsSuccessStatusCode)
+            {
+                var data = await response.Content.ReadAsAsync<IList<CourseComprehension>>();
+                var item = data.FirstOrDefault(i => i.Id == id);
+                var json = JsonConvert.SerializeObject(item, Formatting.None, new JsonSerializerSettings()
+                {
+                    ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
+                });
+                return Json(json);
+            }
+            return Json("Internal Server Error");
+        }
+
+
+
+        // GET: CourseComprehensions/Delete/5
+        public JsonResult Delete(int id)
+        {
+            var client = new HttpClient
+            {
+                BaseAddress = new Uri(Baseurl)
+            };
+            var result = client.DeleteAsync("API/CourseComprehension/" + id).Result;
+            return Json(result);
         }
 
         // POST: CourseComprehensions/Delete/5
