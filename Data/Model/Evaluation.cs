61e2929251d09d40984709519bc01bf11d0d3c7d
Silvia Novita <silvianovita678@gmail.com>
2019-12-29 20:09:01 +0700

    Adding Evaluation in Data

---
+++ Data/Model/Evaluation.cs
@@ -0,0 +1,35 @@
+﻿using Data.Base;
+using Data.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data.Model
+{
+    public class Evaluation : BaseModel
+    {
+        public string Name { get; set; }
+        public int Weight { get; set; }
+
+        public Evaluation() { }
+        public Evaluation(EvaluationVM evaluationVM)
+        {
+            this.Name = evaluationVM.Name;
+            this.Weight = evaluationVM.Weight;
+            this.CreateDate = DateTimeOffset.Now;
+            this.IsDelete = false;
+        }
+        public void Update(EvaluationVM evaluationVM)
+        {
+            this.Name = evaluationVM.Name;
+            this.Weight = evaluationVM.Weight;
+            this.UpdateDate = DateTimeOffset.Now;
+            this.IsDelete = false;
+        }
+        public void Delete()
+        {
+            this.DeleteDate = DateTimeOffset.Now.LocalDateTime;
+            this.IsDelete = true;
+        }
+    }
+}
---
+++ Data/Repositories/EvaluationRepository.cs
@@ -0,0 +1,42 @@
+﻿using Data.Model;
+using Data.Repositories.Interface;
+using Data.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data.Repositories
+{
+    public class EvaluationRepository : IEvaluationRepository
+    {
+        public int Create(EvaluationVM evaluationVM)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Evaluation> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Evaluation Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Evaluation Get(EvaluationVM evaluationVM)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Update(int id, EvaluationVM evaluationVM)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
